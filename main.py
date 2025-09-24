6755fa22cea8ed18888c0d84f6b3e1dab61f4cef
caffin8 <smith.gregory.s@gmail.com>
2025-09-24 14:29:38 -0500

    Added clock and tick

--- main.py
+++ main.py
@@ -5,24 +5,27 @@ import pygame
 from constants import *
 
 def main():
-    print("Starting Asteroids!")
-    print(f"Screen width: {SCREEN_WIDTH}")
-    print(f"Screen height: {SCREEN_HEIGHT}")
     
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
-    
+    clock = pygame.time.Clock()  #Create clock instance
+    dt = 0
     running = True
+    
+
     while running:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
-                return
-
-        screen.fill("black")  # Use the screen object to fill
+                running = False  # Set running to False
+        
+        screen.fill("black")
         pygame.display.flip()
+        
+
+        dt = clock.tick(60) / 1000.0  # Convert to seconds
+    
 
     pygame.quit()
-
 
 if __name__ == "__main__":
     main()
